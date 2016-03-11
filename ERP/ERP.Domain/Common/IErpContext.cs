d37b6b3ea9dc08a09dde125033f7b4a212896cec
arthuraasantos <arthuraasantos@hotmail.com>
2016-03-10 23:47:34 -0300

    Adicionando arquivos

---
+++ ERP/ERP.Domain/Common/IErpContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ERP.Domain.Common
+{
+    public interface IErpContext
+    {
+        Guid Id { get; }
+
+        IOrganization Organization { get; }
+        ILicence Licence { get; set; }
+        IUser User { get; set; }
+        IClient Client { get; set; }
+
+        void Validate();
+    }
+}
---
+++ ERP/ERP.Domain/Common/IUser.cs
@@ -0,0 +1,6 @@
+﻿namespace ERP.Domain.Common
+{
+    public interface IUser
+    {
+    }
+}
