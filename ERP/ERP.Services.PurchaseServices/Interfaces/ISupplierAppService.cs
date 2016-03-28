3e7f857a249650877eca3a5c516df3b3675dbc37
emersonaraujo <mmsr_luis@hotmail.com>
2016-03-28 12:36:00 -0300

    Inclusão do organizationId

--- ERP/ERP.Services.PurchaseServices/Interfaces/ISupplierAppService.cs
+++ ERP/ERP.Services.PurchaseServices/Interfaces/ISupplierAppService.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ERP.Services.PurchaseServices.Dtos.Supplier;
 
 namespace ERP.Services.PurchaseServices.Interfaces
 {
     public interface ISupplierAppService
     {
-        Guid CreateSupplier(SupplierNewDto newSupplier);
+        Guid CreateSupplier(SupplierNewDto newSupplier, Guid organizationId);
     }
 }
